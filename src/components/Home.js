592aa1f9e1f47c892a918b4b00ba2d06c675d8b0
john <johnbabu021@gmail.com>
2021-07-09 11:35:24 +0530

    added all the styles for tesla

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,69 @@ import Section from './Section'
 function Home() {
     return (
         <Container>
-            <Section />
+            <Section
+                title="Model S"
+                description="Order Online for touchless delivery"
+                backgroundImg='model-s.jpg'
+                leftBtnText="Order Online"
+                RightBtnText="Existing Inventory"
+
+
+            />
+            <Section
+                title="Model Y"
+                description="Order Online for touchless delivery"
+                backgroundImg='model-y.jpg'
+                leftBtnText="Order Online"
+                RightBtnText="Existing Inventory"
+
+
+            />
+            <Section
+                title="Model 3"
+                description="Order Online for touchless delivery"
+                backgroundImg='model-3.jpg'
+                leftBtnText="Order Online"
+                RightBtnText="Existing Inventory"
+
+
+            />
+            <Section
+                title="Model X"
+                description="Order Online for touchless delivery"
+                backgroundImg='model-x.jpg'
+                leftBtnText="Order Online"
+                RightBtnText="Existing Inventory"
+
+
+            />
+            <Section
+                title="Lowest Cost Solar Panels in America"
+                description="Money-back guarentee"
+                backgroundImg='solar-panel.jpg'
+                leftBtnText="Order now"
+                RightBtnText="Learn more"
+
+
+            />
+            <Section
+                title="Solar for New Roofs"
+                description="Solar roof costs Less than a new Roof plus Solar panels"
+                backgroundImg='solar-roof.jpg'
+                leftBtnText="Order now"
+                RightBtnText="Learn more"
+
+
+            />
+
+            <Section
+                title="Accessories"
+                backgroundImg='accessories.jpg'
+                leftBtnText="Order now"
+
+
+            />
+
         </Container>
     )
 }
@@ -16,8 +78,4 @@ export default Home
 const Container = styled.div`
 height:100vh;
 width:100vw;
-background-size:cover;
-background-position:center;
-bacround-repeat:no-repeat;
-background-image:url('/images/model-s.jpg');
 `
